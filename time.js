a97ae24972e790cd3011e1398b9fc2220aa7fd6f
shizukani-cp <shizukani-cp@users.noreply.github.com>
2025-02-02 20:15:56 +0000

    deploy: ce5f2c6051d78cb0cc108bf5b599eff2d6493bb0

--- time.js
+++ time.js
@@ -1,3 +1,1 @@
-let update_time = new Date('2025/01/31 18:55');
-update_time.setHours(update_time.getHours() + 9);
-document.getElementById('last_update').innerText = update_time.toLocaleString().slice(0,-3);
+document.getElementById('last_update').innerText = '2025/01/11 09:39'
